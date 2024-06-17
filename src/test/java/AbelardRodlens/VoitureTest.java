c8a6bbca1192df5a309898459f60f5656d621886
ABELARD Rodlens <12207851@crit-xubuntu>
2024-06-17 17:02:55 +0200

    modif

--- src/test/java/AbelardRodlens/VoitureTest.java
+++ src/test/java/AbelardRodlens/VoitureTest.java
@@ -1,5 +1,6 @@
 package AbelardRodlens;
 
+import com.sun.istack.internal.NotNull;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -20,7 +21,47 @@ public class VoitureTest {
         assertTrue(voiture instanceof Voiture);
         assertTrue(voiture.getMarque() instanceof String);
         assertEquals(int.class,voiture.getPrix());
+        assertEquals(voiture.getMarque(),"Aston Martin");
+
             
         }
+
+    @Test
+
+    public void setPrixTest(){
+        Voiture voiture = new Voiture("Audi",15000);
+        voiture.setPrix(10000);
+        assertEquals(voiture.getPrix(),10000);
+
+    }
+
+    @Test
+
+    public void setMarqueTest(){
+        Voiture voiture = new Voiture("Audi",15000);
+        voiture.setMarque("Audi");
+        assertEquals(voiture.getMarque(),"Audi");
+    }
+
+
+    @Test
+
+    public void getMarqueTest(){
+        Voiture voiture = new Voiture("Audi",15000);
+        voiture.setMarque("Audi");
+        assertEquals(voiture.getMarque(),"Audi");
+    }
+
+    @Test
+
+    public void getPrixTest(){
+        Voiture voiture = new Voiture("Audi",15000);
+        assertEquals(15000,voiture.getPrix());
+
+    }
+
+
+
+
         
     }
